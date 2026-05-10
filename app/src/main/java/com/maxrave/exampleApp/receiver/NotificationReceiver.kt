3df1fd6433b5b7de78d4956b21b81d003caa3bfd
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-10 03:25:23 -0300

    Update NotificationReceiver.kt

--- app/src/main/java/com/maxrave/exampleApp/receiver/NotificationReceiver.kt
+++ app/src/main/java/com/maxrave/exampleApp/receiver/NotificationReceiver.kt
@@ -8,11 +8,14 @@ import com.maxrave.exampleApp.player.LocalPlayerManager
 class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         when (intent.action) {
-            "ACTION_PLAY_PAUSE" -> LocalPlayerManager.togglePlayPause()
+            "ACTION_PLAY_PAUSE" -> {
+                // Passamos o context recebido no onReceive
+                LocalPlayerManager.togglePlayPause(context)
+            }
             "ACTION_NEXT" -> LocalPlayerManager.next(context)
             "ACTION_PREVIOUS" -> LocalPlayerManager.previous(context)
             "ACTION_STOP" -> {
-                // Implementar parada total se necessário
+                // Opcional: parar o serviço
             }
         }
     }
