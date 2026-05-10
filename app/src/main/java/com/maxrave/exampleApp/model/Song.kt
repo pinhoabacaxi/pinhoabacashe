2c15756bf8d4e9fca51a58631eb420d3d5049169
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-10 07:28:03 -0300

    Update Song.kt

--- app/src/main/java/com/maxrave/exampleApp/model/Song.kt
+++ app/src/main/java/com/maxrave/exampleApp/model/Song.kt
@@ -1,9 +1,8 @@
 package com.maxrave.exampleApp.model
 
+import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class Song(
     val id: Long,
     val title: String,
@@ -12,4 +11,31 @@ data class Song(
     val duration: Long,
     val uri: String,
     val albumId: Long
-) : Parcelable
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readLong(),
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readLong(),
+        parcel.readString() ?: "",
+        parcel.readLong()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeLong(id)
+        parcel.writeString(title)
+        parcel.writeString(artist)
+        parcel.writeString(album)
+        parcel.writeLong(duration)
+        parcel.writeString(uri)
+        parcel.writeLong(albumId)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<Song> {
+        override fun createFromParcel(parcel: Parcel): Song = Song(parcel)
+        override fun newArray(size: Int): Array<Song?> = arrayOfNulls(size)
+    }
+}
