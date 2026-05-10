39aeb1264f0e7201f852050d56ac4f2fa5ebff87
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-10 07:19:00 -0300

    Update Song.kt

--- app/src/main/java/com/maxrave/exampleApp/model/Song.kt
+++ app/src/main/java/com/maxrave/exampleApp/model/Song.kt
@@ -1,13 +1,15 @@
 package com.maxrave.exampleApp.model
 
-import android.net.Uri
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Song(
     val id: Long,
     val title: String,
     val artist: String,
     val album: String,
     val duration: Long,
-    val uri: Uri,
+    val uri: String,
     val albumId: Long
-)
+) : Parcelable
