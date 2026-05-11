cdf7d1c96e78251821a0272290bbb80f3adad7da
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-11 20:19:34 -0300

    Update DownloadHelper.kt

--- app/src/main/java/com/maxrave/exampleApp/repository/DownloadHelper.kt
+++ app/src/main/java/com/maxrave/exampleApp/repository/DownloadHelper.kt
@@ -9,18 +9,18 @@ import android.widget.Toast
 class DownloadHelper(private val context: Context) {
 
     fun startDownload(title: String, artist: String, url: String) {
-        [span_65](start_span)val fileName = "$artist - $title.mp3".replace("/", "-")[span_65](end_span)
+        val fileName = "$artist - $title.mp3".replace("/", "-")
         
         val request = DownloadManager.Request(Uri.parse(url))
-            [span_66](start_span).setTitle("A baixar: $title")[span_66](end_span)
+            .setTitle("A baixar: $title")
             .setDescription(artist)
             .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-            [span_67](start_span).setDestinationInExternalPublicDir(Environment.DIRECTORY_MUSIC, fileName)[span_67](end_span)
+            .setDestinationInExternalPublicDir(Environment.DIRECTORY_MUSIC, fileName)
             .setAllowedOverMetered(true)
             .setAllowedOverRoaming(true)
 
         val manager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        [span_68](start_span)manager.enqueue(request)[span_68](end_span)
+        manager.enqueue(request)
         
         Toast.makeText(context, "Download iniciado...", Toast.LENGTH_SHORT).show()
     }
