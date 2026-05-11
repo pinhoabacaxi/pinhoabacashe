ac0404fb96ff7b3fb067a4c7f3829cb925eb3f2e
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-10 22:59:06 -0300

    Update PlayerPrefs.kt

--- app/src/main/java/com/maxrave/exampleApp/repository/PlayerPrefs.kt
+++ app/src/main/java/com/maxrave/exampleApp/repository/PlayerPrefs.kt
@@ -5,20 +5,16 @@ import android.content.SharedPreferences
 import com.maxrave.exampleApp.player.LocalPlayerManager
 
 class PlayerPrefs(context: Context) {
-    private val prefs: SharedPreferences = context.getSharedPreferences("player_settings", Context.MODE_PRIVATE)
+    private val prefs: SharedPreferences = context.getSharedPreferences("player_prefs", Context.MODE_PRIVATE)
 
-    fun savePlayerState(isShuffle: Boolean, repeatMode: Int, volume: Float, lastSongId: Long) {
-        prefs.edit().apply {
-            putBoolean("shuffle", isShuffle)
-            putInt("repeat", repeatMode)
-            putFloat("volume", volume)
-            putLong("last_song_id", lastSongId)
-            apply()
+    var isShuffle: Boolean
+        get() = prefs.getBoolean("is_shuffle", false)
+        set(value) = prefs.edit().putBoolean("is_shuffle", value).apply()
+
+    var repeatMode: LocalPlayerManager.RepeatMode
+        get() {
+            val name = prefs.getString("repeat_mode", LocalPlayerManager.RepeatMode.NONE.name)
+            return try { LocalPlayerManager.RepeatMode.valueOf(name!!) } catch (e: Exception) { LocalPlayerManager.RepeatMode.NONE }
         }
-    }
-
-    fun getShuffle(): Boolean = prefs.getBoolean("shuffle", false)
-    fun getRepeatMode(): Int = prefs.getInt("repeat", 0) // 0 = NONE
-    fun getVolume(): Float = prefs.getFloat("volume", 1.0f)
-    fun getLastSongId(): Long = prefs.getLong("last_song_id", -1L)
+        set(value) = prefs.edit().putString("repeat_mode", value.name).apply()
 }
