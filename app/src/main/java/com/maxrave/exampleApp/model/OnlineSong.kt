fcba18e65fc4f763733c3a2852d3ab3d39365f3b
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-10 23:01:06 -0300

    Update OnlineSong.kt

--- app/src/main/java/com/maxrave/exampleApp/model/OnlineSong.kt
+++ app/src/main/java/com/maxrave/exampleApp/model/OnlineSong.kt
@@ -3,7 +3,7 @@ package com.maxrave.exampleApp.model
 data class OnlineSong(
     val videoId: String,
     val title: String,
-    val artist: String,
+    val author: String,
     val thumbnailUrl: String,
     var streamUrl: String? = null // O link extraído virá para aqui
 )
