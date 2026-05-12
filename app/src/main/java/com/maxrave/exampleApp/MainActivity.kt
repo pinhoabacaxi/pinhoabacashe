9e290a94d41e86c9172d41f972400858358bc683
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-12 15:57:01 -0300

    Update MainActivity.kt

--- app/src/main/java/com/maxrave/exampleApp/MainActivity.kt
+++ app/src/main/java/com/maxrave/exampleApp/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var hybridAdapter: HybridAdapter
     private lateinit var musicLoader: MusicLoader
     private lateinit var repository: PlaylistRepository
-    
+    private lateinit var btnOnlineSearch: ImageButton
     private var currentList = mutableListOf<Any>()
     private var filteredList = mutableListOf<Any>()
 
