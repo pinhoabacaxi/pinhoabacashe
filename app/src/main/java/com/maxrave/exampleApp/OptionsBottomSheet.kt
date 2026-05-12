44dcd7e0204008ad7f475b5f3c14fd2b1aedbe4a
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-12 11:22:47 -0300

    Create OptionsBottomSheet.kt

---
+++ app/src/main/java/com/maxrave/exampleApp/OptionsBottomSheet.kt
@@ -0,0 +1,41 @@
+package com.maxrave.exampleApp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class OptionsBottomSheet(
+    private val track: Any,
+    private val onActionSelected: (String) -> Unit
+) : BottomSheetDialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Crie um layout simples chamado layout_options_bottom_sheet.xml com estes IDs
+        val view = inflater.inflate(R.layout.layout_options_bottom_sheet, container, false)
+
+        view.findViewById<TextView>(R.id.btnPlayNext).setOnClickListener {
+            onActionSelected("PLAY_NEXT")
+            dismiss()
+        }
+        
+        view.findViewById<TextView>(R.id.btnAddToQueue).setOnClickListener {
+            onActionSelected("ADD_QUEUE")
+            dismiss()
+        }
+
+        view.findViewById<TextView>(R.id.btnDownloadMp3).setOnClickListener {
+            onActionSelected("DOWNLOAD_MP3")
+            dismiss()
+        }
+
+        view.findViewById<TextView>(R.id.btnAddToPlaylist).setOnClickListener {
+            onActionSelected("ADD_PLAYLIST")
+            dismiss()
+        }
+
+        return view
+    }
+}
