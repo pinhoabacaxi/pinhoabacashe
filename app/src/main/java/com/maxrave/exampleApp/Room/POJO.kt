61fd4c65085710dcbd5ce16fdeb206ed5e750399
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-12 11:04:18 -0300

    Create POJO.kt

---
+++ app/src/main/java/com/maxrave/exampleApp/Room/POJO.kt
@@ -0,0 +1,9 @@
+data class PlaylistWithSongs(
+    @Embedded val playlist: Playlist,
+    @Relation(
+        parentColumn = "playlistId",
+        entityColumn = "songId",
+        associateBy = Junction(PlaylistSongCrossRef::class)
+    )
+    val songs: List<SongEntity>
+)
