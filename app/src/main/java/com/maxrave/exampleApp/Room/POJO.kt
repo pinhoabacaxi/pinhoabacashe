082ede19146c0a5a76dc07f2e2c356c75c79c800
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-12 12:40:45 -0300

    Update POJO.kt

--- app/src/main/java/com/maxrave/exampleApp/Room/POJO.kt
+++ app/src/main/java/com/maxrave/exampleApp/Room/POJO.kt
@@ -1,14 +1,17 @@
 package com.maxrave.exampleApp.Room
 
 import androidx.room.*
-import android.content.Context
 
 data class PlaylistWithSongs(
     @Embedded val playlist: Playlist,
     @Relation(
-        parentColumn = "playlistId",
-        entityColumn = "songId",
-        associateBy = Junction(PlaylistSongCrossRef::class)
+        parentColumn = "id", // ID da Playlist
+        entityColumn = "id", // ID da Música
+        associateBy = Junction(
+            PlaylistSongCrossRef::class,
+            parentColumn = "playlistId",
+            entityColumn = "songId"
+        )
     )
     val songs: List<SongEntity>
 )
