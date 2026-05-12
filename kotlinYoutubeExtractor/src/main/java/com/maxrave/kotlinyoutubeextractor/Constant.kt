42733c2811eb1ba6e537299df9ac7859866e7141
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-11 23:50:15 -0300

    Update Constant.kt

--- kotlinYoutubeExtractor/src/main/java/com/maxrave/kotlinyoutubeextractor/Constant.kt
+++ kotlinYoutubeExtractor/src/main/java/com/maxrave/kotlinyoutubeextractor/Constant.kt
@@ -1,9 +1,14 @@
 package com.maxrave.kotlinyoutubeextractor
 
-class Constant {
+/**
+ * Objeto de constantes globais para o estado da biblioteca.
+ * Alterado para 'object' para facilitar o acesso estático em todo o projeto.
+ */
+object Constant {
     enum class Status {
         PENDING,
         RUNNING,
-        FINISHED
+        FINISHED,
+        ERROR // Adicionado para sincronizar com a lógica de tratamento de falhas do Extrator
     }
 }
