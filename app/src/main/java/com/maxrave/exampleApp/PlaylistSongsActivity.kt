ef8786e876b6376a9bfc5704cfdbee8a41e9d2c1
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-12 09:10:14 -0300

    Update PlaylistSongsActivity.kt

--- app/src/main/java/com/maxrave/exampleApp/PlaylistSongsActivity.kt
+++ app/src/main/java/com/maxrave/exampleApp/PlaylistSongsActivity.kt
@@ -12,6 +12,7 @@ import com.maxrave.exampleApp.repository.FavoriteManager
 import com.maxrave.exampleApp.repository.MusicLoader
 import com.maxrave.exampleApp.repository.PlaylistManager
 import kotlinx.coroutines.launch
+import android.content.Intent
 
 class PlaylistSongsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPlaylistSongsBinding
@@ -19,7 +20,9 @@ class PlaylistSongsActivity : AppCompatActivity() {
     private lateinit var songAdapter: SongAdapter
     private lateinit var playlistManager: PlaylistManager
     private lateinit var musicLoader: MusicLoader
-
+    private lateinit var favoriteManager: FavoriteManager // Certifique-se que esta linha existe
+    // ... resto do código
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlaylistSongsBinding.inflate(layoutInflater)
