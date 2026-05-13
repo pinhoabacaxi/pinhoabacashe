beec5e0688f56ea91024af190327260f057559c7
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-13 18:52:40 -0300

    Update SearchState.kt

--- kotlinYoutubeExtractor/src/main/java/com/maxrave/kotlinyoutubeextractor/SearchState.kt
+++ kotlinYoutubeExtractor/src/main/java/com/maxrave/kotlinyoutubeextractor/SearchState.kt
@@ -1,8 +1,9 @@
 package com.maxrave.kotlinyoutubeextractor
 
+// No módulo extractor, altere para aceitar Any
 sealed class SearchState {
     object Idle : SearchState()
     object Loading : SearchState()
-    data class Success(val results: List<VideoMeta>) : SearchState()
+    data class Success(val results: List<Any>) : SearchState() // Mudar de VideoMeta para Any 
     data class Error(val message: String) : SearchState()
 }
