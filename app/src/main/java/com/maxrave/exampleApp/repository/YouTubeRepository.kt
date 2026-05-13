3e6ee761ea2eb0d5a0f6a3362565f7db1397a238
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-13 19:51:42 -0300

    Update YouTubeRepository.kt

--- app/src/main/java/com/maxrave/exampleApp/repository/YouTubeRepository.kt
+++ app/src/main/java/com/maxrave/exampleApp/repository/YouTubeRepository.kt
@@ -172,10 +172,10 @@ class YouTubeRepository(private val context: Context) {
             ?: "YouTube"
         
         // Tenta pegar a melhor thumbnail disponível
-        val thumbnails = obj.optJSONObject("thumbnail")?.optJSONArray("thumbnails") 
-            ?: obj.optJSONObject("thumbnails")?.optJSONArray(0)?.optJSONArray("thumbnails")
         
         val thumbUrl = thumbnails?.optJSONObject(thumbnails.length() - 1)?.optString("url") ?: ""
+        val thumbnails = obj.optJSONObject("thumbnail")?.optJSONArray("thumbnails") 
+            ?: obj.optJSONObject("thumbnails")?.optJSONArray(0)?.optJSONArray("thumbnails")
         
         // RETORNO COM PARÂMETROS NOMEADOS:
         // Isso garante que cada variável entre no campo correto da data class
