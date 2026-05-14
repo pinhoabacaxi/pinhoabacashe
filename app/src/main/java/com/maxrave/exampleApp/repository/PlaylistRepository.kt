96c0fbb1c5b78316a54b7a6ce50b72227f3b65c2
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-14 01:06:24 -0300

    Update PlaylistRepository.kt

--- app/src/main/java/com/maxrave/exampleApp/repository/PlaylistRepository.kt
+++ app/src/main/java/com/maxrave/exampleApp/repository/PlaylistRepository.kt
@@ -11,7 +11,10 @@ class PlaylistRepository(context: Context) {
     private val dao = AppDatabase.getDatabase(context).musicDao()
 
     // --- PLAYLISTS ---
+    // Adicione no PlaylistRepository.kt
+    fun getAllPlaylistsFlow() = dao.getAllPlaylistsFlow()
 
+    fun getSongsFromPlaylistFlow(playlistId: Long) = dao.getSongsFromPlaylistFlow(playlistId)
     suspend fun createPlaylist(name: String): Long {
         return dao.insertPlaylist(Playlist(name = name))
     }
